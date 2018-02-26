4674c58ee1dc38daa0817cca5dc54b5b1f2b21b2
mike gorecki <michael.c.gorecki@gmail.com>
2018-02-25 22:49:58 -0600

    added start of book controller

---
+++ BookProject.Web/Controllers/BookInfoController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace BookProject.Web.Controllers
+{
+    [Route("bookinfo")]
+    public class BookInfoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Info()
+        {
+            return Ok(Json("UP"));
+        }
+    }
+}
